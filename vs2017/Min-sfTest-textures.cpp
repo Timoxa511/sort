d50b7773ab8ff2e1c49f5468e32fa8d57d64a748
Timoxa511 <tsvinarev@yandex.ru>
2018-09-29 23:42:53 +0300

    а вот и вся картина, бинарных, правда, файлов нет, а вам везет, конечно, вам мусора не увидать

---
+++ vs2017/Min-sfTest-textures.cpp
@@ -0,0 +1,14 @@
+
+#include <SFML/Graphics.hpp>
+#include <assert.h>
+#include <stdlib.h>
+
+int main ()
+    {
+    system ("dir");
+
+    sf::Texture texture;
+    texture.loadFromFile ("1.jpg");
+    }
+
+//это я перепутал сборочки сфмля
